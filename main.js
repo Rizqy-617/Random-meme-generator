9be7c7cbd48690828e5bc687615886815e228c8f
Rizqy-617 <rizqyandriansyah2205@gmail.com>
2023-05-12 15:23:59 +0700

    Add Explanation

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const generateMemeBtn = document.querySelector(
 const memeImage = document.querySelector(".meme-generator img");
 const memeTitle = document.querySelector(".meme-generator .meme-title");
 const memeAuthor = document.querySelector(".meme-generator .meme-author");
+// `document.querySelector` digunakan untuk menangkap selector css atau class yang nanti akan dimanipulasi. Dalam kasus ini, `document.querySelector` digunakan untuk menangkap class `.meme-generator`, `generate-meme-btn`, `meme-title`, dan `meme-author` yang nanti akan dimanipulasi
 
 const updateDetails = (url, title, author) => {
   memeImage.setAttribute("src", url);
@@ -11,14 +12,17 @@ const updateDetails = (url, title, author) => {
   memeAuthor.innerHTML = `Meme by: ${author}`;
 };
 
+// setAttribute disini digunakan untuk menambahkan sebuah nilai baru ke atribute "src" yang ada di html, nilai yang ditambahkan berupa url dinamis yang didapatkan dari API
+//innerHTML ini digunakan untuk menambahkan sebuah nilai baru terhadap content HTML
+
 const generateMeme = () => {
   fetch("https://meme-api.com/gimme/wholesomememes")
-  .then((response) => response.json())
+  .then((response) => response.json()) //mengembalikan sebuah response dalam bentuk json
   .then((data) => {
-    updateDetails(data.url, data.title, data.author);
+    updateDetails(data.url, data.title, data.author); //data json tersebut lalu disimpan dalam argumen data.url, data.title, dana data.author yang nanti bisa digunakan
   });
 };
 
-generateMemeBtn.addEventListener("click", generateMeme);
+generateMemeBtn.addEventListener("click", generateMeme); //setiap kali button dengan class generate-meme-btn diklik maka akan menjalankan function generateMeme
 
 generateMeme();
